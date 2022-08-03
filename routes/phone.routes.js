7f81b3be865bcef04632dea00ee14426a939a9da
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-08-03 13:54:42 +0200

    finish backend

--- routes/phone.routes.js
+++ routes/phone.routes.js
@@ -5,4 +5,14 @@ router.get('/', (req, res, next) => {
     res.json(phonesJson)
 });
 
+router.get('/phone/:id', (req, res, next) => {
+    const {id} = req.params
+
+    phonesJson.findById(id)
+    .then((phone) =>{
+        res.json({phone})
+    })
+    .catch((err) => res.json(err))
+})
+
 module.exports = router;
